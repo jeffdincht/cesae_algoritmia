d47fb52bb4085707a79f74e507ffd8eb272bb354
jeferson <jeffdincht@live.com>
2023-06-26 13:54:42 +0100

    exercicio 2 ficha 04

--- Ficha_04/src/Exercicio_02.java
+++ Ficha_04/src/Exercicio_02.java
@@ -6,17 +6,26 @@ public class Exercicio_02 {
         int num;
 
         do {
-            System.out.println("Digite: \n1 - Criar \n2- Editar \n3-Eliminar \n4-Sair");
+            System.out.println("Digite: \n1 - Criar \n2- Editar \n3- Eliminar \n4- Sair");
             num = input.nextInt();
-        }while(num != 4){
+
             switch (num){
                 case 1:
                     System.out.println("Operacao escolhida: Criar");
+                    break;
                 case 2:
                     System.out.println("Operacao escolhida: Editar");
+                    break;
                 case 3:
                     System.out.println("Operacao escolhida: Eliminar");
+                    break;
+                case 4:
+                    break;
+                default:
+                    System.out.println("Digite uma opcao valida");
             }
+
+        }while(num != 4);
+
         }
     }
-}
